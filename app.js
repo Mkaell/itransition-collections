570cf3eeb8a67a7d40cc483e8a7498217d9ce80b
Mkaell <mrohacevich@yandex.ru>
2022-04-16 15:52:08 +0300

    changed app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const path = require('path')
 const mongoose = require('mongoose');
 
 require('dotenv').config()
@@ -11,6 +11,14 @@ app.use(express.json({ limit: "50mb", extended: true }));
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
